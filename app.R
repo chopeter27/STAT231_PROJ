18810d42b153d5c01fce7875ed766a03331b45dc
chopeter27 <chopeter27@gmail.com>
2018-12-10 17:50:27 -0500

    Fixed the rstudio issue

--- app.R
+++ app.R
@@ -4,9 +4,10 @@ library(dplyr) #manipulating datasets
 library(tidyverse) #general tidying
 library(rpart) #tree modeling for classification
 library(partykit) #tree modeling
-library(qdap) #text data manipulation
-library(RCurl)
-library(XML)
+
+# Word Cloud Visualization
+library(wordcloud) #wordcloud
+library(reshape2) #faceting wordcloud by color
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -24,7 +25,6 @@ ui <- fluidPage(
       mainPanel(
          textOutput("text"),
          verbatimTextOutput("sentiment_words"),
-         plotOutput("tree_plot"),
          plotOutput("wordcloud")
       )
    )
@@ -32,26 +32,25 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
-  
-  tree <- readRDS(file = "tree.rds")
-  
+
+
   # Reactive source that will take in the text and create a data frame with each row being a word
   newData <- reactive({
     text_df <- data_frame(txt = input$text) %>% 
       unnest_tokens(word, txt) 
   }) 
   
-  # output$wordcloud <- renderPlot({
-  #   text_df %>%
-  #     anti_join(stop_words) %>%
-  #     count(word) %>%
-  #     with(wordcloud(words = word, freq = n, max.words = 100, min.freq = 1, random.order=FALSE, rot.per = 0.35, colors = brewer.pal(8, "Dark2")))
-  # 
-  # })
+  output$wordcloud <- renderPlot({
+    newData() %>%
+      anti_join(stop_words) %>%
+      count(word) %>%
+      with(wordcloud(words = word, freq = n, max.words = 100, min.freq = 1, random.order=FALSE, rot.per = 0.35, colors = brewer.pal(8, "Dark2")))
+
+  })
   
   # Print the text that the user input
   output$text <- renderText({
-     input$text
+     head(input$text)
    })
   
   # Print statistics on the number of words, what words are sentiment words, and summarize the information
@@ -69,11 +68,6 @@ server <- function(input, output) {
       summarise(n_positive = sum(score > 0), n_negative = sum(score < 0), avg_score = sum(score)/(n_negative + n_positive))
     print(text_df_summary)
    })
-
-  output$tree_plot <- renderPlot({
-    plot(as.party(tree))
-  })
-  
 }
 
 # Run the application 
